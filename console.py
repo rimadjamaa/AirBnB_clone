19e7b59f6dee8016e0de2046a68532e16b446bd5
rimadjamaa <djamaarima7@gmail.com>
2024-07-15 11:55:13 +0100

    pycodestyle

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
+        print("ByBye")
         return True
 
     def do_EOF(self, arg):
